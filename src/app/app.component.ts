f625c545a5b1b9ca0a69d9b18e9e29ec89deb223
sdjvrensburg <61109456+sdjvrensburg@users.noreply.github.com>
2020-10-15 20:34:42 +0200

    feat(multiple): added methods getAllCampaigns, getCampainByCode, getPromoCodesByCode, getPromosByPromoCodeList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ProductService } from './services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'mweb-assessment';
+  campaigns: any;
+  campaignCodes = ['FTTH-PREPAID', 'FTTH-FREESETUP-FREEROUTER'];
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getAllCampaigns().subscribe((res) => {
-      console.log('campaigns', res);
+    this.productService.campaignSubject.subscribe((cmpArr) => {
+      this.campaigns = cmpArr;
+      this.campaignCodes.forEach((code) => {
+        this.getCampaignByCode(code);
+        this.getPromoCodesByCode(code);
+      });
     });
+    this.productService.getAllCampaigns().subscribe();
+  }
+
+  getCampaignByCode(code): any {
+    console.log('Get campaign by code');
+    const tempObj = this.productService.getCampaignByCode(code);
+    console.log('tempObj', tempObj);
+  }
+
+  getPromoCodesByCode(code): any {
+    console.log('Get promo codes');
+    const tempArr = this.productService.getPromoCodesByCode(code);
+    console.log('tempArr', code, tempArr);
+    this.getPromosByPromoCodeList(tempArr);
+  }
+
+  getPromosByPromoCodeList(arr): void {
+    this.productService.getPromosByPromoCodeList(arr);
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,16 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  campaignURL = `${environment}/campaigns/fibre?channels=120&visibility=public`;
+  campaignURL = `${environment.api.base}/campaigns/fibre?channels=120&visibility=public`;
+  promosURL = `${environment.api.base}/products/promos`;
+  campaignSubject = new ReplaySubject<any[]>();
+  campaignArr = [];
   constructor(private http: HttpClient) {}
 
   getAllCampaigns(): Observable<any> {
-    return this.http.get(this.campaignURL);
+    return this.http.get(this.campaignURL).pipe(
+      map((res) => {
+        return res['campaigns'] as any[];
+      }),
+      map((arr) => {
+        this.campaignArr = arr;
+        this.campaignSubject.next(arr);
+      })
+    );
+  }
+
+  getCampaignByCode(code): any {
+    return this.campaignArr.find((item) => item.code === code);
+  }
+
+  getPromoCodesByCode(code): any[] {
+    return this.getCampaignByCode(code)['promocodes'];
+  }
+
+  getPromosByPromoCodeList(arr): void {
+    this.http.get(`${this.promosURL}/${arr.join(',')}`).subscribe((res) => {
+      console.log('Promos', res);
+    });
   }
 }
